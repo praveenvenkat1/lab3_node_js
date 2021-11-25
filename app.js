e23a991d984d16e00a5c667ad82af1f071217df1
praveenvenkat1 <pv2003@hw.ac.uk>
2021-11-25 22:55:54 +0400

    Final commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
  res.render('index'); //no need for ejs extension
 });
 
-//login and registeration segment
+//login and registration segment
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
@@ -19,15 +19,14 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+//route for login
 app.get("/login", (req, res) => {
     res.render("login");
 });
-  
+//route for contacts
 app.get("/register", (req, res) => {
     res.render("register");
 });
-
-
 //route for contacts
 app.get('/contacts', (req, res) => {
     res.render('contacts');
@@ -39,7 +38,7 @@ app.use(router);
 
 
 //make the app listen on port
-const port = process.argv[2] || process.env.PORT || 3000;
+const port = process.argv[2] || process.env.PORT || 3001;
 const server = app.listen(port, () => {
  console.log(`Cart app listening at http://localhost:${port}`);
 });
